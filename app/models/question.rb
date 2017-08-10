0fabad0939e4e25b8714cce92d24f8e34b588ff9
Ruslan <plexus.9310@gmail.com>
2017-08-10 07:54:09 +0000

    Add relations Question-Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+  
   validates :title, presence: true, length: { minimum: 5, maximum: 50  }
   validates :body,  presence: true, length: { minimum: 5, maximum: 255 }
 end
