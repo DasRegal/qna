ddd6c55d6bf13dee2b9fc39c942803e2afdb1d7a
Ruslan <plexus.9310@gmail.com>
2017-08-10 07:53:17 +0000

    Add test for relations

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body}
   it { should validate_length_of(:body).is_at_most(255) }
+  it { should belong_to(:question) }
 end
