460bcf36b5f26a8ed4f15e1984fca44d927403d9
Ruslan <plexus.9310@gmail.com>
2017-08-10 05:10:28 +0000

    Add test for Answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :body}
+  it { should validate_length_of(:body).is_at_most(255) }
 end
