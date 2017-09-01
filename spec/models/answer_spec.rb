25ead44d9e660091f7723bd14a18c6d0c6ee289d
Ruslan <plexus.9310@gmail.com>
2017-09-01 08:53:19 +0000

    Add Favorite Test for model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body}
   it { should validate_length_of(:body).is_at_most(255) }
   it { should belong_to(:question) }
+  
+  context 'Mark answer to favorite' do
+    let(:question_with_answers) { create(:question, :with_answers) }
+    
+    it 'check funtion' do
+      answer = question_with_answers.answers.first
+      answer.set_favorite
+      
+      expect(answer.is_favorite?).to eq true
+    end
+  end
 end
