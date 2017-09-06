8e3c079932159e17e609dca69f27f097ff3fbdaa
Ruslan <plexus.9310@gmail.com>
2017-09-06 09:43:24 +0000

    Fix some defects

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,14 +5,23 @@ RSpec.describe Answer, type: :model do
   it { should validate_length_of(:body).is_at_most(255) }
   it { should belong_to(:question) }
   
-  context 'Mark answer to favorite' do
+  describe '#set_favorite' do
     let(:question_with_answers) { create(:question, :with_answers) }
     
-    it 'check funtion' do
+    it 'sets favorite flag to true' do
       answer = question_with_answers.answers.first
       answer.set_favorite
       
       expect(answer.is_favorite?).to eq true
+    end
+    
+    it 'resets favorite flag to false' do
+      answer = question_with_answers.answers.first
+      answer.set_favorite
+      answer.set_favorite
+      answer.reload
+      
+      expect(answer.is_favorite?).to eq false
     end
   end
 end
